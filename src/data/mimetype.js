88839bb82f37534723eee96fa04447febed6853a
Ian Effendi <effendiian@gmail.com>
2020-02-18 02:34:55 -0500

    Add support for babel transpilation and nodemon

--- src/data/mimetype.js
+++ src/data/mimetype.js
@@ -2,6 +2,7 @@ module.exports = {
     HTML: 'text/html',
     TEXT: 'text/plain',
     CSS: 'text/css',
+    JS: 'text/javascript',
     XML: 'application/xml',
     JSON: 'application/json',
     PNG: 'image/png',
--- src/router/htmlResponses.js
+++ src/router/htmlResponses.js
@@ -4,11 +4,15 @@ const mimetype = require('./../data/mimetype.js');
 const router = require('./router.js');
 
 // Get the static references.
-const index = fs.readFileSync(`${__dirname}/../../client/client.html`);
-const css = fs.readFileSync(`${__dirname}/../../client/style.css`);
+const index = fs.readFileSync(`${__dirname}/../../hosted/client.html`);
+const bundle = fs.readFileSync(`${__dirname}/../../hosted/bundle.js`);
+const css = fs.readFileSync(`${__dirname}/../../hosted/style.css`);
 
 // Get the index.
 const getIndex = (request, response) => router.sendResponse(request, response, 200, mimetype.HTML, index);
+
+// Get the bundled JS.
+const getBundle = (request, response) => router.sendResponse(request, response, 200, mimetype.JS, bundle);
 
 // Get the CSS.
 const getCSS = (request, response) => router.sendResponse(request, response, 200, mimetype.CSS, css);
@@ -17,12 +21,12 @@ const getCSS = (request, response) => router.sendResponse(request, response, 200
 const routes = {
     '/': getIndex,
     '/index.html': getIndex,
+    '/bundle.js': getBundle,
     '/style.css': getCSS
 };
 
 // Export the routes.
 module.exports = {
     routes,
-    getIndex,
-    getCSS
+    getIndex
 }
